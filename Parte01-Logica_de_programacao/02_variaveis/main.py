2cfaa5bb6cb4a35d2012c9d85a273713b4f9a768
joaosilva0306 <joao.silva295@aluno.senai.br>
2026-07-01 21:17:55 -0300

    Primeiro commit. Aula do dia 01/07/2026.

---
+++ Parte01-Logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome = "João Carlos"
+idade = 19
+altura = 1.80
+programador = False
+
+# saída de dados
+print(nome)
+print(idade)
+print(altura)
+print(programador)
---
+++ Parte01-Logica_de_programacao/04_concatenacao/main.py
@@ -0,0 +1,9 @@
+# declaracão de variaveis
+nome = input("Informe seu nome: ")
+telefone = input("Informe seu telefone: ")
+
+# saída de dados
+print("Olá ", nome, ", e meu telefone é", telefone, ".")
+print("Olá " + nome + ", e meu telefone é " + telefone + ".")
+print("Olá {}, e meu telefone é {}.".format(nome, telefone))
+print(f"Olá {nome}, e meu telefone é {telefone}.")
